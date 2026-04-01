e0699a9fd274db1d5e2abb6c01617742a30511de
kirtan-123 <kirtannv@gmail.com>
2026-04-01 10:04:17 +0530

    Improve student app frontend and serve index page

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Students! Flask App Running Successfully"
+    return send_file("index.html")
 
 @app.route("/about")
 def about():
